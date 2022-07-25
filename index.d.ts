f904826376d59d2c3a770915c4e8a951436195a4
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-25 18:48:53 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -74,15 +74,11 @@ PromiseResult<ValueType>
 >;
 
 /**
-Narrows a variable of type PromiseResult to type PromiseFulfilledResult if the variable has the property 'value', otherwise narrows it to PromiseRejectedResult type.
-@param promiseResult - a variable of type PromiseResult
-@returns boolean
+Narrows a variable of type `PromiseResult` to type `PromiseFulfilledResult` if the variable has the property `value`, otherwise narrows it to the `PromiseRejectedResult` type.
 */
 export function isFulfilled<T>(promiseResult: PromiseResult<T>): promiseResult is PromiseFulfilledResult<T>;
 
 /**
-Narrows a variable of type PromiseResult to type PromiseRejectedResult if the variable has the property 'reason', otherwise narrows it to PromiseFulfilledResult type.
-@param promiseResult - a variable of type PromiseResult
-@returns boolean
+Narrows a variable of type `PromiseResult` to type `PromiseRejectedResult` if the variable has the property `reason`, otherwise narrows it to the `PromiseFulfilledResult` type.
 */
 export function isRejected<T>(promiseResult: PromiseResult<T>): promiseResult is PromiseRejectedResult;
